5c1b50d5eb78f8a90f03e2631c27fc293d635281
Aadil <177419930+AadilPB@users.noreply.github.com>
2026-07-03 00:47:12 -0400

    Add PC and adder modules

---
+++ src/program_counter.v
@@ -0,0 +1,21 @@
+module program_counter
+(
+    input clk;
+    input [31:0] pc_update,
+    input rst,
+    output reg [31:0] pc
+)
+
+always @(posedge clk) 
+begin
+if (rst == 1'b0)
+begin
+    pc <= 32'h00000000
+end
+else
+begin
+    pc <= pc_update;
+end
+
+end
+endmodule
